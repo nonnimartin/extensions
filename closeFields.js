7fff4ee4cf3cf0cd2bcf0d015e70a14ce2abce07
nonnimartin <nonnimartin@gmail.com>
2017-04-04 19:50:59 -0700

    Fixed and cleaned up closeFields.js

--- closeFields.js
+++ closeFields.js
@@ -5,38 +5,36 @@ $(document).ready(function () {
 	  e.initEvent.apply(e, Array.prototype.slice.call(arguments, 1))
 	  target.dispatchEvent(e)
 	};
-    
-    //Clickoff for inside dialog box
-    var resCodeEl = document.getElementById('00NG000000COwcj_ilecell')
-    var clickOff = document.getElementById('section_header')
 
-    //Setting values  - may need to make the selectors more intelligent
-    dispatchMouseEvent(resCodeEl, 'dblclick', true)
-    $( "#00NG000000COwcj option[value='Resolved by TSE']").prop('selected', true);
-    dispatchMouseEvent(clickOff, 'click', true)
-
-    if ($( "#00NG000000COwcj option[value='Resolved by TSE']").prop('selected')) {
+   function setValues() {
         var statusEl       = document.getElementById('cas7_ileinner')
         var statusOkButton = $('input[type="button"][value="OK"]')
         dispatchMouseEvent(statusEl, 'dblclick', true)
         $( "#cas7 option[value='Closed']").prop('selected', true)
         statusOkButton.click()
-    }
 
-    if ($( "#00NG000000COwcj option[value='Resolved by TSE']").prop('selected')) {
         var resEl       = document.getElementById('00NG0000009dNvI_ileinner')
         dispatchMouseEvent(resEl, 'dblclick', true)
         var resolutionText = prompt("Resolution text: ")
         $( "#00NG0000009dNvI").val(resolutionText)
         var resOkButton = $('input[type="button"][value="OK"]')
         resOkButton.click()
-    }
 
-    if ($( "#00NG000000COwcj option[value='Resolved by TSE']").prop('selected')) {
         var prodEl = document.getElementById('Product_ileinner')
         dispatchMouseEvent(prodEl, 'dblclick', true)
         $( "#Product").val('Software Composition Analysis Enterprise Edition')
         dispatchMouseEvent(clickOff, 'click', true)
     }
+
+    //Clickoff for inside dialog box
+    var resCodeEl = document.getElementById('00NG000000COwcj_ilecell')
+    var clickOff = document.getElementById('section_header')
+
+    //Setting values  - may need to make the selectors more intelligent
+
+    setTimeout(function() { dispatchMouseEvent(resCodeEl, 'dblclick', true);}, 50);
+    setTimeout(function() { $( "#00NG000000COwcj option[value='Resolved by TSE']").prop('selected', true);}, 500);
+    setTimeout(function() { dispatchMouseEvent(clickOff, 'click', true);}, 600);
+    setTimeout(function() { setValues();}, 700);
 
 });
